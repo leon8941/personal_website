7951535a4ae0cf2edf9394b8c287dfb90542218c
Leon M <leon8941@gmail.com>
2019-01-20 22:26:41 +0800

    Fix folder files import issues

--- client/src/containers/AboutUs/AboutUs.js
+++ client/src/containers/AboutUs/AboutUs.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import Layout from '../../components/Layout/Layout'
-import ContentBuilder from '../../containers/ContentBuilder/ContentBuilder'
+import ContentBuilder from './ContentBuilder/ContentBuilder'
 
 class AboutUs extends Component {
     render() {
