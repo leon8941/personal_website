3128e86e75ddb00fa7430f1757e894e5298382df
Teck Onn <leon8941@gmail.com>
2020-07-04 23:11:43 +0800

    tweaks router 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
     # protect_from_forgery with: :exception #May need to enable for Live environment
     protect_from_forgery with: :null_session
+
+    def fallback_index_html
+        render :file => 'public/index.html'
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
